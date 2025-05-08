8f85132c040b00887a853e9aef06b570fee77960
OrlandoJim24 <orlandojim2023@gmail.com>
2025-05-08 00:35:46 +0000

    Project finished

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector('.card').classList.add(getRandomSuit());
+  document.querySelector('.card .number').innerHTML = getRandomNumber ();
+};
+
+
+let getRandomNumber = ()=> {
+  let number = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+  let indexNumbers = Math.floor(Math.random() * number.length);
+  return number[indexNumbers];
+};
+
+let getRandomSuit = () => {
+  let suit = ["diamond","spade","heart","club"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
 };
